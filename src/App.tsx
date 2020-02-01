fdb48b872d9f0f528992f7d8532a029461468fc0
Jakub <jakub.bulhak@gmail.com>
2020-02-01 18:03:57 +0100

    Okta config

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,26 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './App.css';
+import SignIn from "./containers/SingIn/SignIn"
 import { Box } from '@material-ui/core/';
+import { Security, ImplicitCallback } from '@okta/okta-react';
+
+const config = {
+  issuer: 'https://${yourOktaDomain}/oauth2/default',
+  redirectUri: window.location.origin + '/implicit/callback',
+  clientId: '{clientId}',
+  pkce: true
+}
 
 
 const App = () => {
+  useEffect(() => {
+    console.log(process.env.REACT_APP_CLIENT_ID)
+  })
   return (
-    <Box component="div">
-      test
-    </Box>
+    <div>
+      <SignIn />
+    </div>
+  
   );
 }
 
